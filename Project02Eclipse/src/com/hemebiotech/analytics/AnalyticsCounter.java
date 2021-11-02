ceafc638c7ce759cfabf0cf2d2c36a58c8552c25
Mel <adjohmelanie@gmail.com>
2021-11-02 10:11:50 +0100

    Gestion de tous les symptômes et affichage du resultat par ordre alphabétique.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,45 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static java.util.Map.Entry.comparingByKey;
+
 
 public class AnalyticsCounter {
 	// private static int headacheCount = 0;
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) throws Exception{
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		Map<String,Integer> symptomsRecap = new HashMap<String, Integer>();
+
 		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("dialated pupils")) {
-				pupilCount++;
+			if (!symptomsRecap.containsKey(line)){
+				symptomsRecap.put(line, 1);
+			} else {
+				Integer symptomValue = symptomsRecap.get(line);
+				symptomsRecap.put(line, symptomValue +1);
 			}
 
 			line = reader.readLine();	// get another symptom
 		}
-		
+
+		Map<String, Integer> result = new TreeMap<String, Integer>(symptomsRecap);
+
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+
+		for (Map.Entry<String, Integer> entry : result.entrySet()) {
+			writer.write(entry.getKey() +": " + entry.getValue() + "\n");
+		}
 		writer.close();
 	}
 }
